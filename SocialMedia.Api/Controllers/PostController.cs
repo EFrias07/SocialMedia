d43f4ffb7cb2c4540bf3e64a4b2360d49c39d0a6
LIGAMD\EFrias <20197657@itla.edu.do>
2023-03-02 13:38:10 -0400

    Add project files.

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        /*Crear mi api*/
+
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            var post = new PostRepository().GetPosts();
+            return Ok(post);
+        }
+    }
+}
