0411b412fb0c81c838c6aab6c85cf6d05f873437
LIGAMD\EFrias <20197657@itla.edu.do>
2023-03-03 15:50:00 -0400

    commit bien

---
+++ SocialMedia.Core/Entities/Comentario.cs
@@ -0,0 +1,19 @@
+﻿using SocialMedia.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace SocialMedia.Infrastructure.Entitites
+{
+    public partial class Comentario
+    {
+        public int IdComentario { get; set; }
+        public int IdPublicacion { get; set; }
+        public int IdUsuario { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Activo { get; set; }
+
+        public virtual Publicacion IdPublicacionNavigation { get; set; }
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+    }
+}
